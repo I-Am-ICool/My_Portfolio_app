c8a4d584359ab73f7ab79dad2942cd9d636cf625
I-Am-ICool <ik.ejike@yahoo.com>
2024-04-09 21:03:24 +0100

    Create columns, image and description

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.jpg")
 
+
 with col2:
     st.title("Ardit Suice")
     content = """Hi, I'm Ikechukwu. Don't mind the picture. I'll put mine
     so many things to write but later. Just testing my skills
     """
     st.info(content)
+
+st.write("Below you can find")
